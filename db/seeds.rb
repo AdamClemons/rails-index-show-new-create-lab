7dc1e2fe6d66ea9ad50d7267dba2103ec7029e4e
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2022-02-09 17:18:04 -0500

    commit tree 3b1b151ba798acc2bf1b31feed0767948cd72cc2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create({coupon_code: '492435', store: 'Walgreens'})
+Coupon.create({coupon_code: '245485343', store: 'CVS'})
